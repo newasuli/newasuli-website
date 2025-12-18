4ebad7e9d0262f000e2c233862a2849f1ed1eedb
Sapat Chaudhary <chy.sapat@gmail.com>
2025-12-18 10:47:19 +0545

    Changes: Link Underline space navbar responsive fix add section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-
-      <div className="min-h-screen">
-
+      <div className="h-screen">
+        <section className="h-1/2 lg:h-screen fixed inset-0 -z-10">
+          <Image src={"/images/home_bg.png"} alt="Newa Suli Restaurant" fill />
+        </section>
+        <section className="w-full h-1/2 lg:h-screen bg-black/10 flex items-center justify-center">
+          <h2 className="text-4xl lg:text-7xl text-gray-50/80 text-center font-newsreader font-bold">
+            Welcome to Newa Suli
+          </h2>
+        </section>
       </div>
+      <section className="w-full h-screen bg-white"></section>
+      <section className="w-full h-screen bg-dark border-y-6 border-red">
+        <div className="bg-gray-400"></div>
+      </section>
+      <section className="w-full h-screen bg-white"></section>
       <div>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3534.724530092683!2d85.33673557617271!3d27.633047028763382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1738dc92c46f%3A0xac25e0e6e6e8c67e!2sHarisiddhi%20Newa%20Suli!5e0!3m2!1sen!2snp!4v1765278955274!5m2!1sen!2snp"
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,12 +33,12 @@ const Navbar = () => {
         </div>
       </Link>
       <div
-        className="lg:hidden flex flex-col gap-2 w-12 z-50 transition-all duration-300 ease-in-out"
+        className="lg:hidden flex flex-col gap-2 w-8 z-50 transition-all duration-300 ease-in-out"
         onClick={toggleMobileNav}
       >
         <div
           className={`w-full h-0.5 bg-gray-500 rounded-lg transition-all ${
-            showMobileNav && "translate-x-0.5 rotate-25 origin-left"
+            showMobileNav && "translate-0.5 rotate-34 origin-left"
           }`}
         ></div>
         <div
@@ -48,12 +48,12 @@ const Navbar = () => {
         ></div>
         <div
           className={`w-full h-0.5 bg-gray-500 rounded-lg transition-all ${
-            showMobileNav && "translate-x-0.5 -rotate-25 origin-left"
+            showMobileNav && "translate-x-0.5 -rotate-34 origin-left"
           }`}
         ></div>
       </div>
       <nav
-        className={`flex flex-col gap-8 px-8 py-24 fixed top-0 right-0 bottom-0 left-0 w-full h-screen z-40 bg-white md:right-0 md:left-auto md:w-1/2 lg:flex-row ${
+        className={`flex flex-col items-center gap-8 px-8 py-20 fixed top-0 right-0 bottom-0 w-1/2 h-screen z-40 bg-white md:right-0 md:left-auto md:w-1/2 lg:flex-row ${
           showMobileNav ? "translate-x-0" : "translate-x-full"
         } transition-all duration-300 delay-100 lg:px-0 lg:scale-x-100 lg:static lg:h-auto lg:w-auto lg:bg-transparent lg:translate-x-0 lg:py-0`}
       >
@@ -64,7 +64,7 @@ const Navbar = () => {
           <li>
             <Link
               href="/"
-              className={`font-poppins font-medium text-xl lg:text-base text-gray-500 ${
+              className={`font-poppins font-medium text-lg lg:text-base text-gray-500 ${
                 pathname === "/" ? "lg:text-white" : ""
               } hover-effect-underline`}
               onNavigate={(e) => {
@@ -77,7 +77,7 @@ const Navbar = () => {
           <li>
             <Link
               href="/about"
-              className={`font-poppins font-medium text-xl lg:text-base text-gray-500 ${
+              className={`font-poppins font-medium text-lg lg:text-base text-gray-500 ${
                 pathname === "/about" ? "lg:text-white" : ""
               } hover-effect-underline`}
               onNavigate={(e) => {
@@ -90,7 +90,7 @@ const Navbar = () => {
           <li>
             <Link
               href="/menu"
-              className={`font-poppins font-medium text-xl lg:text-base text-gray-500 ${
+              className={`font-poppins font-medium text-lg lg:text-base text-gray-500 ${
                 pathname === "/menu" ? "lg:text-white" : ""
               } hover-effect-underline`}
               onNavigate={(e) => {
@@ -103,7 +103,7 @@ const Navbar = () => {
           <li>
             <Link
               href="/gallery"
-              className={`font-poppins font-medium text-xl lg:text-base text-gray-500 ${
+              className={`font-poppins font-medium text-lg lg:text-base text-gray-500 ${
                 pathname === "/gallery" ? "lg:text-white" : ""
               } hover-effect-underline`}
               onNavigate={(e) => {
@@ -114,14 +114,15 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
-        <button className="px-6 py-2 bg-dark text-white font-poppins font-medium text-xl lg:text-base lg:font-normal rounded-lg cursor-pointer">
+        <button className="px-6 py-2 bg-dark text-white font-poppins font-medium text-base lg:font-normal rounded-lg cursor-pointer">
           Contact Us
         </button>
       </nav>
       <div
-        className={`w-screen h-screen absolute inset-0 bg-black/40 transition-all duration-300 ease-in-out ${
+        className={`w-screen h-screen absolute inset-0 bg-black/60 transition-all duration-300 ease-in-out ${
           showMobileNav ? "visible" : "invisible"
         } lg:hidden z-30`}
+        onClick={() => setShowMobileNav(false)}
       ></div>
     </header>
   );
